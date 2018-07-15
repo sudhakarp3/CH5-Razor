65b687db0b46e8767535766d782d679a3e5455da
Sudhakar <sudha2u@gmail.com>
2018-07-15 15:28:19 +0400

    page 117

--- CH5-Razor/Controllers/HomeController.cs
+++ CH5-Razor/Controllers/HomeController.cs
@@ -49,5 +49,15 @@ namespace CH5_Razor.Controllers
 
             return View();
         }
+        public ActionResult DemoArray()
+        {
+            Product[] array = {
+            new Product {Name = "Kayak", Price = 275M},
+            new Product {Name = "Lifejacket", Price = 48.95M},
+            new Product {Name = "Soccer ball", Price = 19.50M},
+            new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            return View(array);
+        }
     }
 }
